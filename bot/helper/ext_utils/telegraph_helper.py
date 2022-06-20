15b585648e0336d2e1ee69b3588fff9bcfeda202
NadifMasamba <96761006+NadifMasamba@users.noreply.github.com>
2022-06-20 23:50:39 +0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -78,4 +78,4 @@ class TelegraphHelper:
         return
 
 
-telegraph=TelegraphHelper('Z-Mirror-Bot', 'https://github.com/shuvam-dawn/MLTB-Custom-UI')
+telegraph=TelegraphHelper('Mirror-CMT-Telegram-Bot', 'https://t.me/RequestMovie84
